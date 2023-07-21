ca6a7603e8f8464bd00b4619f27f5d3f983e9173
Mia4678 <solomia.lutska@gmail.com>
2023-07-21 16:03:12 +0200

    new

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+  const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+
+  let intervalId = 0 ;
+
+  function startColorSwitch() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+
+  function stopColorSwitch() {
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+
+    clearInterval(intervalId);
+  }
+
+  startBtn.addEventListener('click', startColorSwitch);
+  stopBtn.addEventListener('click', stopColorSwitch);
---
+++ src/js/02-timer.js
@@ -0,0 +1,79 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+
+
+
+// Функція для форматування чисел з переданням "0" перед однозначними значеннями
+function addLeadingZero(value) {
+    return value.toString().padStart(2, '0');
+  }
+  
+  // Функція для обчислення різниці часу в мілісекундах між двома датами
+function getTimeDifference(endDate) {
+    const currentTime = new Date().getTime();
+    const targetTime = endDate.getTime();
+    return Math.max(targetTime - currentTime, 0); // Повертаємо 0, якщо дата в минулому
+  }
+
+  function updateTimerDisplay(time) {
+    const { days, hours, minutes, seconds } = convertMs(time);
+    document.querySelector('[data-days]').textContent = addLeadingZero(days);
+    document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+    document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
+    document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
+  }
+
+  // Функція для обчислення днів, годин, хвилин та секунд з мілісекунд
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+
+const startButton = document.querySelector('[data-start]');
+
+// Задаємо параметри для flatpickr
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentTime = new Date().getTime();
+
+    if (selectedDate <= currentTime) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+// Ініціалізуємо flatpickr
+flatpickr("#datetime-picker", options);
+
+// Додаємо обробник події для кнопки "Start"
+startButton.addEventListener('click', () => {
+  const selectedDate = new Date(document.querySelector('#datetime-picker').value);
+  const intervalId = setInterval(() => {
+    const timeDifference = getTimeDifference(selectedDate);
+
+    if (timeDifference === 0) {
+      clearInterval(intervalId);
+    }
+
+    updateTimerDisplay(timeDifference);
+  }, 1000);
+});
